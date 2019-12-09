d03fedb6694376a2b57562c0b7f3d096b924bb27
mor bargig <morbargig@gmail.com>
2019-12-10 00:50:20 +0200

    Updates

---
+++ precache-manifest.e0cf15efd513fd4f7fc14417cd920d06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dacea6b95d62488b399f58139340913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "332278c3e2cd8e422ca3",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "819557917f86b8e7e022",
+    "url": "/static/css/main.c361f948.chunk.css"
+  },
+  {
+    "revision": "332278c3e2cd8e422ca3",
+    "url": "/static/js/2.c9191f9f.chunk.js"
+  },
+  {
+    "revision": "819557917f86b8e7e022",
+    "url": "/static/js/main.e34c91ee.chunk.js"
+  },
+  {
+    "revision": "ea0eb28fac0694a62fd7",
+    "url": "/static/js/runtime~main.61fef8bb.js"
+  }
+]);
